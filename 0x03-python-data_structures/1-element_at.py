4aabc938a43ccc0ff25ba0a524e47f9071736e2f
ellspace <a030.el@gmail.com>
2022-09-26 20:42:14 +0000

    python data structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
-        return None
-    return my_list(idx)
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
+    return None
